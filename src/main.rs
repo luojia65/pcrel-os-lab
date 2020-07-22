a0691a6f549583068985ff22df03dc1df451e1ce
luojia65 <me@luojia.cc>
2020-07-22 18:23:10 +0800

    成功

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn oom(_layout: Layout) -> ! {
 #[naked]
 unsafe fn main() -> ! {
     asm!("
-        auipc   t0, 0   /* t0: start paddr, must align to 2M */
+        auipc   t0, 0   /* t0: start paddr, must align to 4K or 2M */
 
         .option push
         .option norelax
@@ -39,15 +39,21 @@ unsafe fn main() -> ! {
         .option pop
     2:      /* t1: start vaddr */
 
+        /* 
+            If vaddr align to 1G and paddr align to 2M, jump to _start_2M_aligned
+            Or if vaddr align to 2M and paddr align to 4K, jump to _start_4K_aligned
+            Else the program abort
+        */
+
         li      t3, 2 * 1024 * 1024 - 1
         and     t2, t0, t3
-        // beqz    t2, _start_2M_aligned   /* check alignment to 2M */
+        beqz    t2, _start_2M_aligned   /* check alignment to 2M */
         li      t5, 4 * 1024 - 1
         and     t4, t0, t5
         beqz    t4, _start_4K_aligned   /* check alignment to 4K */
     1:  j       1b                      /* Must aligned to 4K, or abort */
     
-    _start_2M_aligned:
+    _start_2M_aligned: /* vaddr: 1G; paddr: 2M */
 
         /*  _start_free + 0  ..= _start_free + 4K:  boot_page_2_paddr 
             _start_free + 4K ..= _start_free + 8K:  boot_page_1_va_paddr 
@@ -109,7 +115,7 @@ unsafe fn main() -> ! {
 
         j       _start_paging
     
-    _start_4K_aligned: 
+    _start_4K_aligned: /* vaddr: 2M; paddr: 4K */
 
     /*  _start_free + 0   ..= _start_free + 4K:  boot_page_2_paddr 
         _start_free + 4K  ..= _start_free + 8K:  boot_page_1_va_paddr 
@@ -121,14 +127,16 @@ unsafe fn main() -> ! {
         
         la      t2, _start_free     
         li      t3, 4096 * 3
-        add     t2, t2, t3          \n/* t2: boot_page_0_va_paddr */
-        srli    t3, t1, 12
-        andi    t3, t3, 0x1FF       \n/* t3: vpn0 */
-        slli    t4, t3, 3           \n/* t4: vpn0 * 8 */
-        add     t5, t4, t2          \n/* t5: boot_page_0[vpn0] */
-        srli    t6, t0, 2
-        ori     t6, t6, 0x0F        \n/* t6: pte entry value, vrwx */
-        sd      t6, 0(t5)
+        add     t2, t2, t3          \n/* t2: current memory address boot_page_0_va_paddr */
+        li      t3, 512 * 8         
+        add     t3, t3, t2          \n/* t3: maximum memory address */
+        srli    t4, t0, 2
+        ori     t4, t4, 0x0F        \n/* t4: current pte entry value, vrwx */
+        li      t5, (1 << 10)       \n/* t5: add to t4 to get next ppn */
+    1:  sd      t4, 0(t2)           \n/* Store pte to memory */    
+        addi    t2, t2, 8           \n/* Prepare for next loop */
+        add     t4, t4, t5
+        bltu    t2, t3, 1b          \n/* Jump to next loop */
 
         la      t2, _start_free     
         li      t3, 4096
