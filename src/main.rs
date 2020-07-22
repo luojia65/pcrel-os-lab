4df578e8382684fbb102c34f30d4151aca5417cd
luojia65 <me@luojia.cc>
2020-07-22 17:59:46 +0800

    Save

--- src/main.rs
+++ src/main.rs
@@ -41,19 +41,19 @@ unsafe fn main() -> ! {
 
         li      t3, 2 * 1024 * 1024 - 1
         and     t2, t0, t3
-        beqz    t2, _start_2M_aligned   /* check alignment to 2M */
+        // beqz    t2, _start_2M_aligned   /* check alignment to 2M */
         li      t5, 4 * 1024 - 1
         and     t4, t0, t5
         beqz    t4, _start_4K_aligned   /* check alignment to 4K */
     1:  j       1b                      /* Must aligned to 4K, or abort */
     
-    _start_4K_aligned:  /* todo */
     _start_2M_aligned:
 
-        /* Load boot page for start_vaddr => start_paddr */
         /*  _start_free + 0  ..= _start_free + 4K:  boot_page_2_paddr 
             _start_free + 4K ..= _start_free + 8K:  boot_page_1_va_paddr 
             _start_free + 8K ..= _start_free + 12K: boot_page_1_pa_paddr */
+
+        /* Load boot page for start_vaddr => start_paddr */
         
         la      t2, _start_free     
         li      t3, 4096
@@ -103,7 +103,106 @@ unsafe fn main() -> ! {
         ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_1, v, leaf */
         sd      t6, 0(t5)
         
+        /* Adjust parameters and start */
+
+        li      a2, 3 * 4096        \n/* a2: start of free space minus boot pages */
+
+        j       _start_paging
+    
+    _start_4K_aligned: 
+
+    /*  _start_free + 0   ..= _start_free + 4K:  boot_page_2_paddr 
+        _start_free + 4K  ..= _start_free + 8K:  boot_page_1_va_paddr 
+        _start_free + 8K  ..= _start_free + 12K: boot_page_1_pa_paddr 
+        _start_free + 12K ..= _start_free + 16K: boot_page_0_va_paddr
+        _start_free + 16K ..= _start_free + 20K: boot_page_0_pa_paddr */
+
+    /* Load boot page for start_vaddr => start_paddr */
+        
+        la      t2, _start_free     
+        li      t3, 4096 * 3
+        add     t2, t2, t3          \n/* t2: boot_page_0_va_paddr */
+        srli    t3, t1, 12
+        andi    t3, t3, 0x1FF       \n/* t3: vpn0 */
+        slli    t4, t3, 3           \n/* t4: vpn0 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_0[vpn0] */
+        srli    t6, t0, 2
+        ori     t6, t6, 0x0F        \n/* t6: pte entry value, vrwx */
+        sd      t6, 0(t5)
+
+        la      t2, _start_free     
+        li      t3, 4096
+        add     t2, t2, t3          \n/* t2: boot_page_1_va_paddr */
+        srli    t3, t1, 21
+        andi    t3, t3, 0x1FF       \n/* t3: vpn1(start_vaddr) */
+        slli    t4, t3, 3           \n/* t4: vpn1 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_1[vpn2] */
+        la      t6, _start_free     
+        li      s0, 4096 * 3
+        add     t6, t6, s0          \n/* t6: boot_page_0_va_paddr */
+        srli    t6, t6, 2
+        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_1, v, leaf */
+        sd      t6, 0(t5)
+        
+        la      t2, _start_free     \n/* t2: boot_page_2_paddr */
+        srli    t3, t1, 30
+        andi    t3, t3, 0x1FF       \n/* t3: vpn2(start_vaddr) */
+        slli    t4, t3, 3           \n/* t4: vpn2 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_2[vpn2] */
+        la      t6, _start_free     
+        li      s0, 4096
+        add     t6, t6, s0          \n/* t6: boot_page_1_va_paddr */
+        srli    t6, t6, 2
+        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_1, v, leaf */
+        sd      t6, 0(t5)
+
+        /* Load boot page for start_paddr => start_paddr */
+        
+        la      t2, _start_free     
+        li      t3, 4096 * 4
+        add     t2, t2, t3          \n/* t2: boot_page_0_pa_paddr */
+        srli    t3, t0, 12
+        andi    t3, t3, 0x1FF       \n/* t3: vpn0 */
+        slli    t4, t3, 3           \n/* t4: vpn0 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_0[vpn1] */
+        srli    t6, t0, 2
+        ori     t6, t6, 0x0F        \n/* t6: pte entry value, vrwx */
+        sd      t6, 0(t5)
+        
+        la      t2, _start_free     
+        li      t3, 4096 * 2
+        add     t2, t2, t3          \n/* t2: boot_page_1_pa_paddr */
+        srli    t3, t0, 21
+        andi    t3, t3, 0x1FF       \n/* t3: vpn1 */
+        slli    t4, t3, 3           \n/* t4: vpn1 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_1[vpn1] */
+        la      t6, _start_free     
+        li      s0, 4096 * 4
+        add     t6, t6, s0          \n/* t6: boot_page_0_paddr */
+        srli    t6, t6, 2
+        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_0, v, leaf */
+        sd      t6, 0(t5)
+
+        la      t2, _start_free     \n/* t2: boot_page_2_paddr */
+        srli    t3, t0, 30
+        andi    t3, t3, 0x1FF       \n/* t3: vpn2 */
+        slli    t4, t3, 3           \n/* t4: vpn2 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_2[vpn2] */
+        la      t6, _start_free     
+        li      s0, 4096 * 2
+        add     t6, t6, s0          \n/* t6: boot_page_1_paddr */
+        srli    t6, t6, 2
+        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_1, v, leaf */
+        sd      t6, 0(t5)
+
+        /* Adjust parameters and start */
+
+        li      a2, 5 * 4096        \n/* a2: start of free space minus boot pages */
+
+    _start_paging:
+
         /* Write boot page address into satp and refresh */
+
         srli    t2, t2, 12          \n/* t2: boot_page_2_ppn */
         li      t3, 8 << 60         \n/* t3: mode (Sv39) */
         or      t4, t2, t3          \n/* t4: satp value */
