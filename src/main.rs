283e5d51230631a2b466b2a7e62fbaae735cb81c
luojia65 <me@luojia.cc>
2020-07-22 21:30:02 +0800

    Small fix

--- src/main.rs
+++ src/main.rs
@@ -140,13 +140,15 @@ unsafe fn main() -> ! {
     
     _start_4K_aligned: /* vaddr: 2M; paddr: 4K */
 
-    /*  _start_free + 0   ..= _start_free + 4K:  boot_page_2_paddr 
+    /*  
+        _start_free + 0   ..= _start_free + 4K:  boot_page_2_paddr 
         _start_free + 4K  ..= _start_free + 8K:  boot_page_1_va_paddr 
-        _start_free + 8K  ..= _start_free + 12K: boot_page_1_pa_paddr 
         _start_free + 12K ..= _start_free + 16K: boot_page_0_va_paddr
-        _start_free + 16K ..= _start_free + 20K: boot_page_0_pa_paddr */
+        _start_free + 16K ..= _start_free + 20K: boot_page_0_pa_paddr 
+        _start_free + 8K  ..= _start_free + 12K: boot_page_1_pa_paddr 
+    */
 
-    /* Load boot page for start_paddr => start_paddr */
+    /* L0 boot page for start_paddr => start_paddr */
 
         la      t2, _start_free     
         li      t3, 4096 * 4
@@ -154,39 +156,13 @@ unsafe fn main() -> ! {
         srli    t3, t0, 12
         andi    t3, t3, 0x1FF       \n/* t3: vpn0 */
         slli    t4, t3, 3           \n/* t4: vpn0 * 8 */
-        add     t5, t4, t2          \n/* t5: boot_page_0[vpn1] */
+        add     t5, t4, t2          \n/* t5: boot_page_0[vpn0] */
         srli    t6, t0, 2
         ori     t6, t6, 0x0F        \n/* t6: pte entry value, vrwx */
         sd      t6, 0(t5)
-        
-        la      t2, _start_free     
-        li      t3, 4096 * 2
-        add     t2, t2, t3          \n/* t2: boot_page_1_pa_paddr */
-        srli    t3, t0, 21
-        andi    t3, t3, 0x1FF       \n/* t3: vpn1 */
-        slli    t4, t3, 3           \n/* t4: vpn1 * 8 */
-        add     t5, t4, t2          \n/* t5: boot_page_1[vpn1] */
-        la      t6, _start_free     
-        li      s0, 4096 * 4
-        add     t6, t6, s0          \n/* t6: boot_page_0_paddr */
-        srli    t6, t6, 2
-        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_0, v, leaf */
-        sd      t6, 0(t5)
 
-        la      t2, _start_free     \n/* t2: boot_page_2_paddr */
-        srli    t3, t0, 30
-        andi    t3, t3, 0x1FF       \n/* t3: vpn2 */
-        slli    t4, t3, 3           \n/* t4: vpn2 * 8 */
-        add     t5, t4, t2          \n/* t5: boot_page_2[vpn2] */
-        la      t6, _start_free     
-        li      s0, 4096 * 2
-        add     t6, t6, s0          \n/* t6: boot_page_1_paddr */
-        srli    t6, t6, 2
-        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_1, v, leaf */
-        sd      t6, 0(t5)
+    /* L0 boot page for start_vaddr => start_paddr */
 
-    /* Load boot page for start_vaddr => start_paddr */
-        
         la      t2, _start_free     
         li      t3, 4096 * 3
         add     t2, t2, t3          \n/* t2: current memory address boot_page_0_va_paddr */
@@ -199,6 +175,8 @@ unsafe fn main() -> ! {
         addi    t2, t2, 8           \n/* Prepare for next loop */
         add     t4, t4, t5
         bltu    t2, t3, 1b          \n/* Jump to next loop */
+
+    /* page1va.vpn1 = page0va; page2.vpn2 = page1va */
 
         la      t2, _start_free     
         li      t3, 4096
@@ -226,6 +204,80 @@ unsafe fn main() -> ! {
         ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_1, v, leaf */
         sd      t6, 0(t5)
 
+    /* If paddr.vpn2 equals vaddr.vpn2, then create only one L1 boot page */
+
+        srli    t2, t0, 30
+        and     t2, t2, 0x1FF       \n/* t2: vpn2 of start_paddr */
+        srli    t3, t1, 30
+        and     t3, t3, 0x1FF       \n/* t3: vpn2 of start_vaddr */
+        beq     t2, t3, _4K_create_one_l1_page
+        
+    /* _4K_create_two_l1_pages */
+
+    /* page1pa.vpn1 = page0pa; page2.vpn2 = page1pa */
+
+        la      t2, _start_free     
+        li      t3, 4096 * 2
+        add     t2, t2, t3          \n/* t2: boot_page_1_pa_paddr */
+        srli    t3, t0, 21
+        andi    t3, t3, 0x1FF       \n/* t3: vpn1 */
+        slli    t4, t3, 3           \n/* t4: vpn1 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_1[vpn1] */
+        la      t6, _start_free     
+        li      s0, 4096 * 4
+        add     t6, t6, s0          \n/* t6: boot_page_0_pa_paddr */
+        srli    t6, t6, 2
+        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_0, v, leaf */
+        sd      t6, 0(t5)
+
+        la      t2, _start_free     \n/* t2: boot_page_2_paddr */
+        srli    t3, t0, 30
+        andi    t3, t3, 0x1FF       \n/* t3: vpn2 */
+        slli    t4, t3, 3           \n/* t4: vpn2 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_2[vpn2] */
+        la      t6, _start_free     
+        li      s0, 4096 * 2
+        add     t6, t6, s0          \n/* t6: boot_page_1_pa_paddr */
+        srli    t6, t6, 2
+        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_1, v, leaf */
+        sd      t6, 0(t5)
+        
+        j       _4K_l1_page_finish
+
+    _4K_create_one_l1_page:
+
+    /* If paddr.vpn[1,2] equals vaddr.vpn[1,2], but paddr.vpn0 != vaddr.vpn0, this is not valid. */
+
+        srli    t2, t0, 21
+        and     t2, t2, 0x1FF       \n/* t2: vpn1 of start_paddr */
+        srli    t3, t1, 21
+        and     t3, t3, 0x1FF       \n/* t3: vpn1 of start_vaddr */
+        bne     t2, t3, 2f          \n/* vpn1 != vpn1, break; vpn1 == vpn1, continue */
+        srli    t4, t0, 12
+        and     t4, t4, 0x1FF       \n/* t4: vpn1 of start_paddr */
+        srli    t5, t1, 12
+        and     t5, t5, 0x1FF       \n/* t5: vpn1 of start_vaddr */
+    1:  bne     t4, t5, 1b          \n/* vpn0 != vpn0, loop; vpn0 == vpn0, break */
+    2:
+        
+    /* page1va.ppn1 = page0pa */
+        
+        la      t2, _start_free     
+        li      t3, 4096
+        add     t2, t2, t3          \n/* t2: boot_page_1_va_paddr */
+        srli    t3, t0, 21
+        andi    t3, t3, 0x1FF       \n/* t3: vpn1 */
+        slli    t4, t3, 3           \n/* t4: vpn1 * 8 */
+        add     t5, t4, t2          \n/* t5: boot_page_1[vpn1] */
+        la      t6, _start_free     
+        li      s0, 4096 * 4
+        add     t6, t6, s0          \n/* t6: boot_page_0_pa_paddr */
+        srli    t6, t6, 2
+        ori     t6, t6, 0x01        \n/* t6: pte entry value, ->boot_page_0, v, leaf */
+        sd      t6, 0(t5)
+
+    _4K_l1_page_finish:
+
         /* Adjust parameters and start */
 
         li      a2, 5 * 4096        \n/* a2: start of free space minus boot pages */
@@ -234,6 +286,7 @@ unsafe fn main() -> ! {
 
         /* Write boot page address into satp and refresh */
 
+        la      t2, _start_free     \n/* t2: boot_page_2_paddr */
         srli    t2, t2, 12          \n/* t2: boot_page_2_ppn */
         li      t3, 8 << 60         \n/* t3: mode (Sv39) */
         or      t4, t2, t3          \n/* t4: satp value */
